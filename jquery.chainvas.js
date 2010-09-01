c7fba071d60bb7f9bec8543b2dbddf2a059dbb47
James Daniels <james@marginleft.com>
2010-09-01 15:35:46 -0400

    Complex args and rotation

--- jquery.chainvas.js
+++ jquery.chainvas.js
@@ -28,21 +28,26 @@ jQuery(function($) {
       switch(args.length) {
         case 1:
           // Use a setter if only one argument is present
-          return ' = '+args[0];
+          return ' = '+JSON.stringify(args[0]);
         default:
           // Use a function if there are/aren't arguments
-          return '('+Array.prototype.slice.call(args).join(',')+')';
+          return '('+JSON.stringify(Array.prototype.slice.call(args)).replace(/[$\[|\]^]/g, '')+')';
       }
     }
 
     // pass the function to the dom object and return the jquery object
     var context_pass = function(method, args) {
+      //console.log('ctx.' + method + right_side(args));
       eval('ctx.' + method + right_side(args)); 
       return $ctx;
     };
 
+    $ctx.rotate = function(radians) {
+      ctx.rotate(radians); return $ctx;
+    }
+
     // register jquery responders for all the dom functions for a canvas
-    $('arc arcTo beginPath bezierCurveTo clearRect clearShadow clip closePath createImageData createLinearGradient createPattern createRadialGradient drawImage drawImageFromRect fill fillRect fillStyle fillText font getImageData globalAlpha globalCompositeOperation isPointInPath lineCap lineJoin lineTo lineWidth measureText miterLimit moveTo putImageData quadraticCurveTo rect restore rotate save scale setAlpha setCompositeOperation setFillColor setLineCap setLineJoin setLineWidth setMiterLimit setShadow setStrokeColor setTransform shadowBlur shadowColor shadowOffsetX shadowOffsetY stroke strokeRect strokeStyle strokeText textAlign textBaseline transform translate'.split(' ')).each(function(i) {
+    $('arc arcTo beginPath bezierCurveTo clearRect clearShadow clip closePath createImageData createLinearGradient createPattern createRadialGradient drawImage drawImageFromRect fill fillRect fillStyle fillText font getImageData globalAlpha globalCompositeOperation isPointInPath lineCap lineJoin lineTo lineWidth measureText miterLimit moveTo putImageData quadraticCurveTo rect restore save scale setAlpha setCompositeOperation setFillColor setLineCap setLineJoin setLineWidth setMiterLimit setShadow setStrokeColor setTransform shadowBlur shadowColor shadowOffsetX shadowOffsetY stroke strokeRect strokeStyle strokeText textAlign textBaseline transform translate'.split(' ')).each(function(i) {
       eval('$ctx.'+this+'= function() { return context_pass(\''+this+'\', arguments); }');
     });
 
